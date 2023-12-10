b64d7711a7d2ac1834d87e63d08c0656796eade5
fkthat <fkthat@yandex.ru>
2023-12-10 19:46:16 +0300

    Main API

---
+++ src/FakeItEasy.AutoFake/AutoFaker.cs
@@ -0,0 +1,96 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace FakeItEasy.AutoFake;
+
+/// <inheritdoc/>
+public sealed class AutoFaker
+{
+    private readonly IObjectFactory _factory;
+    private readonly IServiceContainer _container;
+
+    /// <summary>
+    /// Initializes an instance of <see cref="AutoFaker"/>.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public AutoFaker()
+    {
+        // temporary
+        _factory = null!;
+        _container = null!;
+    }
+
+    internal AutoFaker(IServiceContainer container, IObjectFactory factory)
+    {
+        _container = container;
+        _factory = factory;
+    }
+
+    /// <summary>
+    /// Adds an instance to the container.
+    /// </summary>
+    /// <param name="type">The type of service to use.</param>
+    /// <param name="service">The service to use</param>
+    public void Use(Type type, object service)
+    {
+        ArgumentNullException.ThrowIfNull(type, nameof(type));
+        ArgumentNullException.ThrowIfNull(service, nameof(service));
+
+        _container.Use(type, service);
+    }
+
+    /// <summary>
+    /// Adds an instance to the container.
+    /// </summary>
+    /// <typeparam name="T">The type that the instance will be registered as.</typeparam>
+    /// <param name="service">The instance to register.</param>
+    public void Use<T>(T service) where T : class => Use(typeof(T), service);
+
+    /// <summary>
+    /// Searches and retrieves an object from the container that matches the <paramref
+    /// name="type"/>. This can be a service setup explicitly via <see cref="Use"/> or implicitly
+    /// with <see cref="CreateInstance"/>.
+    /// </summary>
+    /// <param name="type">The type of service to retrieve.</param>
+    public object Get(Type type)
+    {
+        ArgumentNullException.ThrowIfNull(type, nameof(type));
+
+        return _container.TryGet(type, out var service) ? service
+            : throw new InvalidOperationException($"Cannot resolve the {type} service.");
+    }
+
+    /// <summary>
+    /// Searches and retrieves an object from the container that matches the <typeparamref
+    /// name="T"/>. This can be a service setup explicitly via <see cref="Use"/> or implicitly with
+    /// <see cref="CreateInstance"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of service to retrieve.</typeparam>
+    public T Get<T>() => (T)Get(typeof(T));
+
+    /// <summary>
+    /// Constructs an instance from known services. Any dependencies (constructor arguments) are
+    /// fulfilled by searching the container or, if not found, automatically generating fakes.
+    /// </summary>
+    /// <param name="type">A concrete type.</param>
+    /// <returns>
+    /// An instance of type with all constructor arguments derived from services setup in the
+    /// container.
+    /// </returns>
+    public object CreateInstance(Type type)
+    {
+        ArgumentNullException.ThrowIfNull(type, nameof(type));
+
+        return _factory.CreateInstance(type);
+    }
+
+    /// <summary>
+    /// Constructs an instance from known services. Any dependencies (constructor arguments) are
+    /// fulfilled by searching the container or, if not found, automatically generating fakes.
+    /// </summary>
+    /// <typeparam name="T">A concrete type.</typeparam>
+    /// <returns>
+    /// An instance of T with all constructor arguments derived from services setup in the
+    /// container.
+    /// </returns>
+    public T CreateInstance<T>() where T : class => (T)CreateInstance(typeof(T));
+}
---
+++ test/FakeItEasy.AutoFake.Tests/AutoFakerTests.cs
@@ -0,0 +1,128 @@
+﻿namespace FakeItEasy.AutoFake;
+
+file interface IFoo { }
+
+file class Foo : IFoo { }
+
+public class AutoFakerTests
+{
+    [Fact]
+    public void Use_checks_args()
+    {
+        var container = A.Fake<IServiceContainer>();
+        var factory = A.Fake<IObjectFactory>();
+
+        var type = typeof(IFoo);
+        var service = new Foo();
+
+        AutoFaker sut = new(container, factory);
+
+        sut.Invoking(s => s.Use(type: null!, service)).Should()
+            .Throw<ArgumentNullException>().WithParameterName(nameof(type));
+
+        sut.Invoking(s => s.Use(type, service: null!)).Should()
+            .Throw<ArgumentNullException>().WithParameterName(nameof(service));
+
+        sut.Invoking(s => s.Use<IFoo>(service: null!)).Should()
+            .Throw<ArgumentNullException>().WithParameterName(nameof(service));
+    }
+
+    [Fact]
+    public void Use_calls_container()
+    {
+        var container = A.Fake<IServiceContainer>();
+        var factory = A.Fake<IObjectFactory>();
+
+        var type = typeof(IFoo);
+        var service = new Foo();
+
+        AutoFaker sut = new(container, factory);
+        sut.Use(type, service);
+        sut.Use<IFoo>(service);
+
+        A.CallTo(() => container.Use(type, service))
+            .MustHaveHappened(2, Times.Exactly);
+    }
+
+    [Fact]
+    public void Get_checks_args()
+    {
+        var container = A.Fake<IServiceContainer>();
+        var factory = A.Fake<IObjectFactory>();
+
+        var type = typeof(IFoo);
+
+        AutoFaker sut = new(container, factory);
+
+        sut.Invoking(s => s.Get(type: null!)).Should()
+            .Throw<ArgumentNullException>().WithParameterName(nameof(type));
+    }
+
+    [Fact]
+    public void Get_returns_from_container()
+    {
+        var container = A.Fake<IServiceContainer>();
+        var factory = A.Fake<IObjectFactory>();
+
+        var type = typeof(IFoo);
+        var service = new Foo();
+
+        object? svc;
+        A.CallTo(() => container.TryGet(type, out svc))
+            .Returns(true).AssignsOutAndRefParameters(service);
+
+        AutoFaker sut = new(container, factory);
+
+        sut.Get(type).Should().Be(service);
+        sut.Get<IFoo>().Should().Be(service);
+    }
+
+    [Fact]
+    public void Get_handles_unsuccess()
+    {
+        var container = A.Fake<IServiceContainer>();
+        var factory = A.Fake<IObjectFactory>();
+
+        var type = typeof(IFoo);
+
+        object? svc;
+        A.CallTo(() => container.TryGet(type, out svc)).Returns(false);
+
+        AutoFaker sut = new(container, factory);
+
+        sut.Invoking(s => s.Get(type)).Should()
+           .Throw<InvalidOperationException>();
+
+        sut.Invoking(s => s.Get<IFoo>()).Should()
+           .Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void CreateInstance_checks_args()
+    {
+        var container = A.Fake<IServiceContainer>();
+        var factory = A.Fake<IObjectFactory>();
+
+        AutoFaker sut = new(container, factory);
+
+        Type type = null!;
+        sut.Invoking(s => s.CreateInstance(type)).Should()
+           .Throw<ArgumentNullException>().WithParameterName(nameof(type));
+    }
+
+    [Fact]
+    public void CreateInstance_returns_from_factory()
+    {
+        var container = A.Fake<IServiceContainer>();
+        var factory = A.Fake<IObjectFactory>();
+
+        var type = typeof(Foo);
+        var instance = new Foo();
+        A.CallTo(() => factory.CreateInstance(type)).Returns(instance);
+
+        AutoFaker sut = new(container, factory);
+
+        sut.CreateInstance(type).Should().Be(instance);
+        sut.CreateInstance<Foo>().Should().Be(instance);
+    }
+}
